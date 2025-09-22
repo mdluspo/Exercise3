f8cb8d29eda1a65e79aa67a356c782bfe8e16a79
shynaperez <perezshynalee@gmail.com>
2025-09-22 00:38:38 -0700

    try this 2

---
+++ feature-ui.py
@@ -0,0 +1,42 @@
+#feature-ui
+tasks=[]
+
+def addtask(task) :
+    tasks.append(task)
+    print("task added!")
+
+def showTasks( ):
+    if len(tasks)==0 :
+        print("no tasks yet")
+    else:
+        for i in range (len(tasks)):
+            print(i+1,".",tasks[i])
+      
+def removetask(tasknumber):
+    tasks.pop(tasknumber) 
+    print("Task removed!!")
+
+def main():
+    while True:
+        print("=======================================")
+        print(">>> TO DO MENU: <<<")
+        print("   1.Add Task")
+        print("   2.Show Tasks")
+        print("   3.Remove Task")
+        print("   4.Exit")
+        ch = input("Enter number of action to execute: ")
+        print("=======================================")
+        if ch=="1":
+            t = input("Enter task: ")
+            addtask(t)
+        elif ch=="2":
+            print("Here's the list of your tasks:\n")
+            showTasks()
+        elif ch=="3":
+            n = int(input("Enter task no to remove: "))
+            removetask(n)   
+        elif ch=="4":
+            break;
+        else:
+            print("Invalid input!!")
+main()
